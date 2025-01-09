6bf3111e7e49473b7430462d6636de771672fe0f
JuanArboledaL <juanarboledalopez@gmail.com>
2025-01-09 13:42:36 +0100

    T6
    09/01/2025-Tema6

---
+++ TemasClaseAndroid/Tema6/EjemploFragmento2/app/src/main/java/com/example/ejemplofragmentos2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ejemplofragmentos2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager FM = getSupportFragmentManager();
+        FragmentTransaction FT = FM.beginTransaction();
+
+        Fragmento fragmento = new Fragmento();
+        FT.add(R.id.contenedor,fragmento);
+        FT.commit();
+
+    }
+}
